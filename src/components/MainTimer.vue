13c12dbef5f43893d109e02a278c26bea0e7fa73
qr <593747881@qq.com>
2024-08-01 09:54:00 +0800

    add timer.

--- src/components/MainTimer.vue
+++ src/components/MainTimer.vue
@@ -1,29 +1,49 @@
 <script setup>
 import {computed, ref} from "vue";
 import moment from "moment";
-  const baseDateTime = ref(moment(new Date()));
-  const curDateTime = ref(moment(new Date()));
-  const calcDateDiff = computed(()=>curDateTime.value.diff(baseDateTime.value, 'seconds'));
-  function start(){
-    console.log("test");
-    debugger;
-    setInterval(() => {
-      baseDateTime.value = moment(new Date()); // 每次都创建一个新的字符串
-    }, 1000);
 
-  }
+/**
+ * 计时组件
+ * 通过时间间隔计算，获取时间差秒数。
+ * 当前时间 - 基础时间 = 经过秒数
+ *
+ * @type {Ref<UnwrapRef<moment.Moment>>}
+ */
+const summaryTimes = ref(0);
+const intervalId = ref(0);
+const status = ref(0);
+// 计算经过秒数
+const times = computed(() => {
+  return moment.utc(summaryTimes.value).format('HH:mm:ss');});
+
+function start(){
+  status.value = 1;
+  intervalId.value = setInterval(() => {
+    summaryTimes.value+=1000;
+  }, 1000);
+}
+function stop(){
+  status.value = 2;
+  clearInterval(intervalId.value);
+}
+function end(){
+  status.value = 0;
+  clearInterval(intervalId.value);
+  summaryTimes.value = 0;
+}
 </script>
 
 <template>
   <div>
-    <div>{{ calcDateDiff }}</div>
-    <button @click="start">开始</button>
-    <button>停止</button>
+    <div>{{ times }}</div>
+    <button :disabled="!(status === 0 || status === 2)" @click="start">开始</button>
+    <button :disabled="!(status === 1 || status === 2)" @click="stop">暂停</button>
+    <button :disabled="!(status !== 0)" @click="end">停止</button>
   </div>
 </template>
 
 <style scoped>
-  div{
-    display: block;
-  }
+div {
+  display: block;
+}
 </style>
