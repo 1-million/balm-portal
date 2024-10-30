9c8f999f7160671571c52fc3421842808a76f79e
qianrui <593747881@qq.com>
2024-10-30 17:37:42 +0800

    【变更配置】修改后端服务地址。

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://8.140.27.43:9999', // 后端服务的地址
+        target: 'http://1.117.69.195:9999', // 后端服务的地址
         changeOrigin: true, // 是否改变源
         pathRewrite: { '^/api': '/api' }, // 路径重写规则
       },
